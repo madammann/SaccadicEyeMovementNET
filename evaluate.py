9fae2bb29feb9b6fe4ebe953d9ce4f2935596ef5
ArghaSarker <argha.sarker.93@gmail.com>
2022-03-28 21:15:53 +0200

    visualization of image patches

--- evaluate.py
+++ evaluate.py
@@ -1,8 +1,12 @@
 '''Module imports'''
 
 '''Library imports'''
+
+import cv2 as cv
+from matplotlib import pyplot as plt
+import sys
 import numpy as np
-import matplotlib.pyplot as plt
+import colorsys
 
 def load_performance_data():
     '''Loads in stored performance data.'''
@@ -34,3 +38,34 @@ def webcam_movement_record():
 def compare_movement():
     '''Analyses movement of human and net by comparison'''
     pass
+
+def hsv2rgb(h,s,v):
+
+  '''for changing the hsv values into BGR'''
+  return tuple(round(i * 255) for i in colorsys.hsv_to_rgb(h,s,v))
+
+
+def visualize_image_patches(image, cordinates, patch_count=10):
+    ''' for visualizing the patches feeding into the lst acordingly'''
+
+    count = patch_count  # integer
+
+    # creates the hsv_scale for changing the colors
+    hsv_scale = np.linspace(0.1, 0.9, count + 2)
+    cordinates = cordinates  # sould be a list of tuples of integers(x and y axis values)
+    thickness = 5
+    for i in range(0, len(cordinates)):
+        color = hsv2rgb(hsv_scale[i], 1, 1)
+
+        x, y = cordinates[i]
+        start_point = (x - 16, y + 16)
+        end_point = (x + 16, y - 16)
+
+        image = cv.rectangle(image, start_point, end_point, color, thickness)
+
+        plt.imshow(image[..., ::-1])  # to display the image without color distorntion
+        plt.axis('off')
+        plt.show()
+
+        #returns the last image with axis
+    return plt.imshow(image[..., ::-1])
